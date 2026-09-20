65455d5f410ea24594047c661b525dfb3346447c
RageQuicks <4sf89prmbd@privaterelay.appleid.com>
2026-09-20 00:26:21 -0500

    Fix client command API for 26.1.2

--- src/client/java/com/fruitfly/client/BrainViewCommands.java
+++ src/client/java/com/fruitfly/client/BrainViewCommands.java
@@ -17,8 +17,8 @@ import net.minecraft.network.chat.Component;
 import java.util.List;
 import java.util.Locale;
 
-import static net.fabricmc.fabric.api.client.command.v2.ClientCommandManager.argument;
-import static net.fabricmc.fabric.api.client.command.v2.ClientCommandManager.literal;
+import static net.fabricmc.fabric.api.client.command.v2.ClientCommands.argument;
+import static net.fabricmc.fabric.api.client.command.v2.ClientCommands.literal;
 
 /**
  * Client-side /brainview commands: pin the live brain map, choose and lock the fly it shows.
