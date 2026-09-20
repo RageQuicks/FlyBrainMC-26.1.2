601e3958d9dbe3b97d15324510813ff1f2e75c10
RageQuicks <4sf89prmbd@privaterelay.appleid.com>
2026-09-20 00:31:18 -0500

    Add shared fly renderer textures

---
+++ src/client/java/com/fruitfly/client/render/FlyRendererTexture.java
@@ -0,0 +1,10 @@
+package com.fruitfly.client.render;
+
+import com.fruitfly.FruitFlyMod;
+import net.minecraft.resources.Identifier;
+
+final class FlyRendererTexture {
+    static final Identifier MALE = FruitFlyMod.id("textures/entity/fruit_fly.png");
+    static final Identifier FEMALE = FruitFlyMod.id("textures/entity/fruit_fly_female.png");
+    private FlyRendererTexture() {}
+}
